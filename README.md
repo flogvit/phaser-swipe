f5e4a6d3726fc7922908c622723244addc91c4ae
flogvit <Vegard.Hanssen@menneske.no>
2015-11-24 21:27:36 +0100

    First release

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # phaser-swipe
 A swipe component for Phaser.io
+
+## Install
+
+You can grab the swipe.js file and include it in your project, or you can use npm or bower:
+
+```bash
+   npm install phaser-swipe
+   bower install phaser-swipe
+```
+
+## Usage
+
+You can use it in two ways. With or without a model. This is without model
+
+```javascript
+  var Swipe = require('phaser-swipe');
+  
+  // in create
+  this.swipe = new Swipe(this.game);
+  
+  // in update
+  var direciton = this.swipe.check();
+  if (direction!==null) {
+    // direction= { x: x, y: y, direction: direction }
+    switch(direction.direction) {
+       case this.swipe.DIRECTION_LEFT: // do something
+       case this.swipe.DIRECTION_RIGHT:
+       case this.swipe.DIRECTION_UP:
+       case this.swipe.DIRECTION_DOWN:
+       case this.swipe.DIRECTION_UP_LEFT:
+       case this.swipe.DIRECTION_UP_RIGHT:
+       case this.swipe.DIRECTION_DOWN_LEFT:
+       case this.swipe.DIRECTION_DOWN_RIGHT:
+    }
+  }
+```
+
+This is with a model. Here you define your methods in your model. Only those methods defined will be used
+So if you do not want the diagonals, you can just omit those methods.
+
+```javascript
+   function YourModel() {
+      up: function(point) {},
+      down: function(point) {},
+      left: function(point) {},
+      right: function(point) {},
+      upLeft: function(point) {},
+      upRight: fuction(point) {},
+      downLeft: function(point) {},
+      downRight: fuction(point) {}
+   };
+   
+   // in create
+   this.swipe = new Swipe(this.game, yourmodel);
+   
+   // in update. The methods will only be called if you have a swipe.
+   // point: { x: x, y: y }
+   this.swipe.check();
+```
---
+++ swipe.js
@@ -0,0 +1,100 @@
+/**
+ * Created by flogvit on 2015-11-03.
+ *
+ * @copyright Cellar Labs AS, 2015, www.cellarlabs.com, all rights reserved
+ * @file
+ * @license MIT
+ * @author Vegard Hanssen <Vegard.Hanssen@cellarlabs.com>
+ *
+ */
+
+
+function Swipe(game, model) {
+  var self = this;
+
+  self.DIRECTION_UP = 1;
+  self.DIRECTION_DOWN = 2;
+  self.DIRECTION_LEFT = 4;
+  self.DIRECTION_RIGHT = 8;
+  self.DIRECTION_UP_RIGHT = 16;
+  self.DIRECTION_UP_LEFT = 32;
+  self.DIRECTION_DOWN_RIGHT = 64;
+  self.DIRECTION_DOWN_LEFT = 128;
+
+  self.game = game;
+  self.model = model !== undefined ? model : null;
+  self.dragLength = 20;
+  self.swiping = false;
+
+  this.game.input.onDown.add(function () {
+    self.swiping = true;
+  });
+  this.game.input.onUp.add(function () {
+    self.swiping = false;
+  })
+}
+
+Swipe.prototype.check = function () {
+  if (!this.swiping) return null;
+
+  if (Phaser.Point.distance(this.game.input.activePointer.position, this.game.input.activePointer.positionDown) < this.dragLength + 20) return null;
+
+  this.swiping = false;
+
+  var direction = null;
+  var deltaX = this.game.input.activePointer.position.x - this.game.input.activePointer.positionDown.x;
+  var deltaY = this.game.input.activePointer.position.y - this.game.input.activePointer.positionDown.y;
+
+  var result = {
+    x: this.game.input.activePointer.positionDown.x,
+    y: this.game.input.activePointer.positionDown.y
+  };
+
+  var deltaXabs = Math.abs(deltaX);
+  var deltaYabs = Math.abs(deltaY);
+
+  if (deltaXabs > this.dragLength && deltaYabs > this.dragLength) {
+    if (deltaX > 0 && deltaY > 0) {
+      direction = this.DIRECTION_DOWN_RIGHT;
+      this.model !== null && this.model.downRight && this.model.downRight(result);
+    } else if (deltaX > 0 && deltaY < 0) {
+      direction = this.DIRECTION_UP_RIGHT;
+      this.model !== null && this.model.upRight && this.model.upRight(result);
+    } else if (deltaX < 0 && deltaY > 0) {
+      direction = this.DIRECTION_DOWN_LEFT;
+      this.model !== null && this.model.downLeft && this.model.downLeft(result);
+    } else if (deltaX < 0 && deltaY < 0) {
+      direction = this.DIRECTION_UP_LEFT;
+      this.model !== null && this.model.upLeft && this.model.upLeft(result);
+    }
+  } else if (deltaXabs > this.dragLength || deltaYabs > this.dragLength) {
+    if (deltaXabs > deltaYabs) {
+      if (deltaX > 0) {
+        direction = this.DIRECTION_RIGHT;
+        this.model !== null && this.model.right && this.model.right(result);
+      } else if (deltaX < 0) {
+        direction = this.DIRECTION_LEFT;
+        this.model !== null && this.model.left && this.model.left(result);
+      }
+    } else {
+      if (deltaY > 0) {
+        direction = this.DIRECTION_DOWN;
+        this.model !== null && this.model.down && this.model.down(result);
+      } else if (deltaY < 0) {
+        direction = this.DIRECTION_UP;
+        this.model !== null && this.model.up && this.model.up(result);
+      }
+    }
+  }
+  if (direction !== null) {
+    result['direction'] = direction;
+    return result;
+  }
+  return null;
+}
+
+if (typeof exports !== 'undefined') {
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Swipe;
+  }
+}
